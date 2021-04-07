c2f989e4f37c065b5456ab1f47820fa30ce346df
Matthew <mattpenguiy@gmail.com>
2021-04-07 22:29:01 +0800

    GDX Setup

--- core/src/com/mygdx/game/Con.java
+++ core/src/com/mygdx/game/Con.java
@@ -2,4 +2,6 @@ package com.mygdx.game;
 
 public class Con {
     public static String GROUND_FLOOR_MAP = "iteration 1 - Ground floor.tmx";
+    public static int WIDTH = 560;
+            public static int HEIGHT = 384;
 }
--- core/src/com/mygdx/game/Screens/GroundFloor.java
+++ core/src/com/mygdx/game/Screens/GroundFloor.java
@@ -1,10 +1,16 @@
 package com.mygdx.game.Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.Con;
 import com.mygdx.game.Main;
 
 public class GroundFloor implements Screen {
@@ -16,14 +22,23 @@ public class GroundFloor implements Screen {
     private TmxMapLoader mapLoader; //helps load the map
     private TiledMap map; //the loaded map object
     private OrthogonalTiledMapRenderer renderer; //renders the map
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
     public GroundFloor(Main game){
         this.myGame = game;
         mapLoader = new TmxMapLoader(); //create an instance of built-in map loader object
-        map = mapLoader.load("levelOne.tmx"); //using map loader object, load the tiled map that you made
+        map = mapLoader.load(Con.GROUND_FLOOR_MAP); //using map loader object, load the tiled map that you made
         renderer = new OrthogonalTiledMapRenderer(map); //render the map.
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Con.WIDTH, Con.HEIGHT, camera);
+        camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0); //set initial camera position
     }
-
+    private void update(float dt)
+    {
+        camera.update();
+        renderer.setView(camera); //sets the view from our camera so it would render only what our camera can see.
+    }
 
     @Override
     public void show() {
@@ -32,14 +47,24 @@ public class GroundFloor implements Screen {
 
     @Override
     public void render(float delta) {
+        update(delta);
+
+        //clear screen
+        Gdx.gl.glClearColor(0, 1, 1 ,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
         myGame.getBatch().begin();
-        myGame.getBatch().draw(stair,0,0);
+
         myGame.getBatch().end();
+        myGame.getBatch().setProjectionMatrix(camera.combined); //updates our batch with our Camera's view and projection matrices.
+
     }
 
     @Override
-    public void resize(int width, int height) {
-
+    public void resize(int width, int height)
+    {
+        viewport.update(width, height); //viewport gets adjusted accordingly
     }
 
     @Override
@@ -59,6 +84,7 @@ public class GroundFloor implements Screen {
 
     @Override
     public void dispose() {
-
+        map.dispose();
+        renderer.dispose();
     }
 }
